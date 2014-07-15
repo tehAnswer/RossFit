23de77ef679d7e41d64d8d69fce73b8e027d1d70
Sergio G <sergiorodriguezgijon@gmail.com>
2014-07-15 18:35:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RossfitApi::Application.config.secret_key_base = '0633b3f1114997b9f8300e94821f710ec4b2eea7dd27759ec1527843b48d5f2e07eea44542eb0aea7e109eebf5c6506aef27141c292d954b1565b7b20c60cef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RossfitApi::Application.config.session_store :cookie_store, key: '_rossfit_api_session'
