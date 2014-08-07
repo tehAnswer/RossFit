9d330d271985f859eb4cbf321fa0abd247b85a79
Sergio G <sergiorodriguezgijon@gmail.com>
2014-08-07 17:45:39 +0200

    Root element in create user json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.register
-			render json: @user, status: 201
+			render json: @user, root: true, status: 201
 		else
 			render json: @user.errors, status: 422
 		end
